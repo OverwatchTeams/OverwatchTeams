910855e388d2a91bc38a7ec83da0eebce5ce0082
kuble <cw6280@gmail.com>
2025-05-22 20:38:42 +0900

    [feat] 맵 검색기능 추가
    - 맵의 한글 검색 기능을 추가했습니다.

--- Assets/Scripts/Controller/PanelController/FindMapPanelController.cs
+++ Assets/Scripts/Controller/PanelController/FindMapPanelController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,7 +12,8 @@ public class FindMapPanelController : PanelController
     private string _path;
 
     public Action<string> OnMapClicked;
-    [SerializeField] private TMP_Text _mapNameInputField;
+    [SerializeField] private TMP_InputField _mapNameInputField;
+    [SerializeField] private TMP_Text _mapNameInputFieldText;
     [SerializeField] private Button _addMapButton;
     [SerializeField] private TMP_Dropdown _dropdown;
     [SerializeField] private GameObject _mapPrefab;
@@ -24,6 +27,29 @@ public class FindMapPanelController : PanelController
     
     private List<GameObject> _maps = new List<GameObject>();
 
+    private string _prevText = "";
+    
+    void Update()
+    {
+        if (_mapNameInputField.isFocused)
+        {
+            string current = _mapNameInputFieldText.text;
+            if (_prevText != current)
+            {
+                _prevText = current;
+
+                Debug.Log("Changed");
+                StringBuilder sb = new StringBuilder(_mapNameInputFieldText.text);
+                sb.Replace("<u>", "");
+                sb.Replace("</u>", "");
+                sb.Replace(" ", "");
+                // TMP_InputField에 강제로 자모를 추가하고 싶다면
+                OnTypingMapName(sb.ToString());
+            }
+
+        }
+    }
+    
     private void OnEnable()
     {
         if (DataManager.instance.isLoadDone)
@@ -35,6 +61,7 @@ public class FindMapPanelController : PanelController
             throw new Exception("맵 정보가 동기화 되지 않았습니다.");
         }
         _newMapType.onValueChanged.AddListener(OnClickAddTypeButton);
+        _mapNameInputField.onValueChanged.AddListener(OnTypingMapName);
     }
     
     private void Initialize()
@@ -75,6 +102,32 @@ public class FindMapPanelController : PanelController
         _addMapButton.onClick.AddListener(OnClickAddMap);
     }
 
+    private void OnTypingMapName(string value)
+    {
+        foreach (var map in _maps)
+        {
+            string mapName = map.GetComponentInChildren<TMP_Text>().text;
+            if (!string.IsNullOrWhiteSpace(mapName) && !string.IsNullOrWhiteSpace(value))
+            {
+                mapName = mapName.Trim().ToLowerInvariant();
+                value = value.Trim().ToLowerInvariant();
+                value = value.Replace("\u200B", "");
+                if (mapName.Contains(value))
+                {
+                    map.SetActive(true);
+                }
+                else
+                {
+                    map.SetActive(false);
+                }
+            }
+            if (value == "")
+            {
+                map.SetActive(true);
+            }
+        }
+    }
+    
     /// <summary>
     /// 맵 선택 시
     /// </summary>
