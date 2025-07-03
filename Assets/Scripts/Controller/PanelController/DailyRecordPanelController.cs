9d664dc47a04b4d7e0a0a0db8836fd6ad0229b97
kuble <cw6280@gmail.com>
2025-07-03 22:33:56 +0900

    [feat] Daily WinRate표 전용 프리팹, 스크립트 분리 및 승차, 연승 정보 추가

--- Assets/Scripts/Controller/PanelController/DailyRecordPanelController.cs
+++ Assets/Scripts/Controller/PanelController/DailyRecordPanelController.cs
@@ -15,6 +15,12 @@ public class DailyRecordPanelController : PanelController
     [SerializeField] private List<ScrollRect> _scrollRects;
     [SerializeField] private TMP_Text _totalRound;
     [SerializeField] private TMP_Text _participantCount;
+    [SerializeField] private Button _gapButton;
+    [SerializeField] private Button _streakButton;
+
+    private List<RefinedMatchData> _matchDatas;
+    private string _sortedType;
+    private Dictionary<string, int> _playerStreaks;
     
     private void OnEnable()
     {
@@ -25,6 +31,7 @@ public class DailyRecordPanelController : PanelController
     {
         base.Initialize();
         InitializeListeners();
+        _sortedType = "gap";
         StartCoroutine(GetDropDownDates());
     }
     
@@ -33,6 +40,10 @@ public class DailyRecordPanelController : PanelController
         base.InitializeListeners();
         _dateDropdown.onValueChanged.RemoveListener(OnDropDownValueChanged);
         _dateDropdown.onValueChanged.AddListener(OnDropDownValueChanged);
+        _gapButton.onClick.RemoveListener(OnGapButtonClicked);
+        _gapButton.onClick.AddListener(OnGapButtonClicked);
+        _streakButton.onClick.RemoveListener(OnStreakButtonClicked);
+        _streakButton.onClick.AddListener(OnStreakButtonClicked);
     }
 
     private void InitializePanel()
@@ -66,24 +77,45 @@ public class DailyRecordPanelController : PanelController
     
     private IEnumerator SetWinRateContainer(string date)
     {
+        //연승 데이터 계산
+        _playerStreaks = GetPlayerStreaks();
+        
+        //랭크 불러오기 승률
+        yield return StartCoroutine(DataController.instance.GetMainDailyData(date));
+        UpdateWinRateContainer(date);
+    }
+
+    private void UpdateWinRateContainer(string date)
+    {
         //랭크표 컨테이너 초기화
         foreach (Transform child in _winRateRankContainer.transform)
         {
             Destroy(child.gameObject);
         }
-
-        //랭크 불러오기 승률
+        
         int i = 1;
-        yield return StartCoroutine(DataController.instance.GetMainDailyData(date));
-
-        var sortedRole =  DataController.instance.DailyGameData.leaderBoard.byDay[date].winRate.total
-            .OrderByDescending(map => map.Value.wins - map.Value.losses)
-            .ToList();
-       
-        foreach (var rank in sortedRole)
+        var sortedWinRate = new List<KeyValuePair<string, WinRateData.WinRate.PlayerWinRate>>();
+        if (_sortedType == "gap")
+        {
+            sortedWinRate =  DataController.instance.DailyGameData.leaderBoard.byDay[date].winRate.total
+                .OrderByDescending(map => map.Value.wins - map.Value.losses)
+                .ToList();   
+        }
+        else if (_sortedType == "streak")
+        {
+            var sortedStreaks = _playerStreaks
+                .OrderByDescending(pair => pair.Value)
+                .ToList();
+            foreach (var streak in sortedStreaks)
+            {
+                sortedWinRate.Add(new KeyValuePair<string, WinRateData.WinRate.PlayerWinRate>(streak.Key, DataController.instance.DailyGameData.leaderBoard.byDay[date].winRate.total[streak.Key]));
+            }
+        }
+        
+        foreach (var rank in sortedWinRate)
         {
             GameObject go = Instantiate(_winRateRankPrefab, _winRateRankContainer.transform);
-            WinRateRankPrefab rankInfo = go.GetComponent<WinRateRankPrefab>();
+            DailyWinRateRankPrefab rankInfo = go.GetComponent<DailyWinRateRankPrefab>();
             if (i % 2 == 0)
             {
                 foreach (var image in rankInfo.gameObject.GetComponentsInChildren<Image>())
@@ -99,43 +131,45 @@ public class DailyRecordPanelController : PanelController
                 }
             }
 
-            if (rank.Value.wins - 1 > rank.Value.losses && i % 2 == 0)
+            rankInfo._rank.text = i.ToString();
+            rankInfo._name.text = rank.Key;
+            rankInfo._gap.text = (rank.Value.wins - rank.Value.losses).ToString();
+            rankInfo._streak.text = _playerStreaks[rank.Key].ToString();
+            int winRate = (int)Mathf.Round(rank.Value.winRate);
+            rankInfo._winRate.text = winRate.ToString();
+            rankInfo._win.text = rank.Value.wins.ToString();
+            rankInfo._lose.text = rank.Value.losses.ToString();
+            
+            if (i % 2 == 0 && 
+                (_sortedType == "gap" && int.Parse(rankInfo._gap.text) > 1 || 
+                 _sortedType == "streak" && int.Parse(rankInfo._streak.text) > 1))
             {foreach (var image in rankInfo.gameObject.GetComponentsInChildren<Image>())
                 {
                     image.sprite = Resources.Load<Sprite>("Images/backGround_table07");
                 }
             }
-            else if (rank.Value.wins - 1 > rank.Value.losses && i % 2 == 1)
+            else if(i % 2 == 1 && 
+                    (_sortedType == "gap" && int.Parse(rankInfo._gap.text) > 1 || 
+                     _sortedType == "streak" && int.Parse(rankInfo._streak.text) > 1)) 
             {foreach (var image in rankInfo.gameObject.GetComponentsInChildren<Image>())
                 {
                     image.sprite = Resources.Load<Sprite>("Images/backGround_table06");
                 }
             }
-            else if (rank.Value.losses - 1 > rank.Value.wins && i % 2 == 0)
+            else if (i % 2 == 0 && 
+                     (_sortedType == "gap" && int.Parse(rankInfo._gap.text) < -1 || 
+                      _sortedType == "streak" && int.Parse(rankInfo._streak.text) < -1))
             {foreach (var image in rankInfo.gameObject.GetComponentsInChildren<Image>())
                 {
                     image.sprite = Resources.Load<Sprite>("Images/backGround_table09");
                 }
             }
-            else if (rank.Value.losses - 1 > rank.Value.wins && i % 2 == 1)
+            else if (i % 2 == 1 && 
+                     (_sortedType == "gap" && int.Parse(rankInfo._gap.text) < -1 || 
+                      _sortedType == "streak" && int.Parse(rankInfo._streak.text) < -1))
             {foreach (var image in rankInfo.gameObject.GetComponentsInChildren<Image>())
                 {
                     image.sprite = Resources.Load<Sprite>("Images/backGround_table08");
-                }
-            }
-            rankInfo._rank.text = i.ToString();
-            rankInfo._name.text = rank.Key;
-            //하드코딩 수정 필요(재사용 하느라 순서가 변경되어있음)
-            rankInfo._lose.text = rank.Value.wins.ToString();
-            rankInfo._winRate.text = rank.Value.losses.ToString();
-            rankInfo._win.text = (rank.Value.wins - rank.Value.losses).ToString();
-            int winRate = (int)Mathf.Round(rank.Value.winRate);
-            rankInfo._draw.text = winRate.ToString();
-            if (int.Parse(rankInfo._win.text) >= 3 || int.Parse(rankInfo._win.text) <= -3)
-            {
-                foreach (var text in rankInfo.gameObject.GetComponentsInChildren<TMP_Text>())
-                {
-                    text.fontStyle = FontStyles.Underline;
                 }
             }
             i++;
@@ -150,22 +184,37 @@ public class DailyRecordPanelController : PanelController
         }
         
         bool isSucceed = false;
-        List<RefinedMatchData> matchDatas = new List<RefinedMatchData>();
-        yield return StartCoroutine(MatchDataManager.instance.GetDailyMatches(success => isSucceed = success, datas => matchDatas = datas,  date));
-        yield return StartCoroutine(MainDataManager.instance.GetDropDownDates(success => isSucceed = success, DataController.instance.OnDropdownDatesLoaded));
-        _totalRound.text = matchDatas.Count.ToString();
-        foreach (var match in matchDatas)
+        yield return StartCoroutine(MatchDataManager.instance.GetDailyMatches(success => isSucceed = success, datas => _matchDatas = datas,  date));
+        _totalRound.text = _matchDatas.Count.ToString();
+        foreach (var match in _matchDatas)
         {
             GameObject go = Instantiate(_dailyMatchPrefab, _dailyMatchContainer.transform);
             DailyMatchPrefab matchObject = go.GetComponent<DailyMatchPrefab>();
             matchObject.SetDailyMatchPrefab(match);
         }
     }
+
+    private void OnGapButtonClicked()
+    {
+        _sortedType = "gap";
+        UpdateWinRateContainer(_dateDropdown.options[_dateDropdown.value].text);
+    }
+
+    private void OnStreakButtonClicked()
+    {
+        _sortedType = "streak";
+        UpdateWinRateContainer(_dateDropdown.options[_dateDropdown.value].text);
+    }
     private void OnDropDownValueChanged(int value)
     {
         string date = _dateDropdown.options[value].text;
-        StartCoroutine(SetWinRateContainer(date));
-        StartCoroutine(SetDailyMatchContainer(date));
+        StartCoroutine(GetDailyInform(date));
+    }
+
+    private IEnumerator GetDailyInform(string date)
+    {
+        yield return StartCoroutine(SetDailyMatchContainer(date));
+        yield return StartCoroutine(SetWinRateContainer(date));
     }
     
     private IEnumerator GetDropDownDates()
@@ -181,4 +230,48 @@ public class DailyRecordPanelController : PanelController
         InitializePanel();
         OnDropDownValueChanged(_dateDropdown.value);
     }
+    
+    public Dictionary<string, int> GetPlayerStreaks()
+    {
+        var sortedMatches = _matchDatas.OrderByDescending(md => md.round).ToList();
+        Dictionary<string, int> streaks = new Dictionary<string, int>();
+
+        foreach (var match in sortedMatches)
+        {
+            for (int i = 0; i < match.players.Count; i++)
+            {
+                string playerName = match.players[i].Item1;
+                bool isBlueTeam = i < 5;
+                bool didWin = (isBlueTeam && match.winner == WinnerTeam.블루) ||
+                              (!isBlueTeam && match.winner == WinnerTeam.레드);
+                bool isDraw = match.winner == WinnerTeam.무승부;
+
+                if (isDraw) continue; // 무승부는 패스
+
+                int currentResult = didWin ? 1 : -1;
+
+                if (!streaks.ContainsKey(playerName))
+                {
+                    streaks[playerName] = currentResult;
+                }
+                else
+                {
+                    int prevStreak = streaks[playerName];
+                    // 같은 방향이면 누적
+                    if ((prevStreak > 0 && currentResult > 0) || (prevStreak < 0 && currentResult < 0))
+                    {
+                        streaks[playerName] += currentResult;
+                    }
+                    else
+                    {
+                        // 방향 다르면 중단
+                        continue;
+                    }
+                }
+            }
+        }
+
+        return streaks;
+    }
+
 }
---
+++ Assets/Scripts/Prefab/DailyRecord/DailyWinRateRankPrefab.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DailyWinRateRankPrefab : MonoBehaviour
+{
+    public TMP_Text _rank;
+    public TMP_Text _name;
+    public TMP_Text _gap;
+    public TMP_Text _streak;
+    public TMP_Text _winRate;
+    public TMP_Text _win;
+    public TMP_Text _lose;
+}
