9582fb28679f21db31f9c80ff5c3d3323f35cb77
kuble <cw6280@gmail.com>
2025-07-11 11:17:17 +0900

    [feat] 플레이어 버튼에 음성, 유저 점수, 연승횟수등 표기

--- Assets/Scripts/Prefab/Button/PlayerButtonPrefab.cs
+++ Assets/Scripts/Prefab/Button/PlayerButtonPrefab.cs
@@ -1,58 +1,174 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class PlayerButtonPrefab : MonoBehaviour
+public class PlayerButtonPrefab : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] PlayerData playerData;
-    [SerializeField] private Animator _animator;
-    //[SerializeField] private SwipdownTextAnimation _swipdownTextAnimation;
+
     [SerializeField] private TMP_Text _name;
     public PlayerData PlayerData => playerData;
-
-    public PlayerData SetPlayerData(PlayerData newPlayerData)
-    {
-        //StopAnimation();
-        playerData = newPlayerData;
-        _name.text = newPlayerData.player;
-        /*List<string> playerNames = new List<string>();
-        playerNames.Add(playerData.player);
-        if (playerData.subNames != null)
-        {
-            foreach (var pName in playerData.subNames)
-            {
-                playerNames.Add(pName);
-            }   
-        }
-        _swipdownTextAnimation.SetTexts(playerNames);
-        StartAnimation();*/
-        return playerData;
-    }
-
-    /*private void StartAnimation()
-    {
-        if (playerData == null) return;
-        if (playerData.subNames == null || playerData.subNames.Count == 0) return;
-        if(_animator.enabled == false)
-        {
-            _animator.enabled = true;
-        }
-        _animator.Play("Anim_SwipdownText",0,0);   
-    }
-    private void StopAnimation()
-    {
-        if (playerData.subNames == null || playerData.subNames.Count == 0) return;
-        _animator.enabled = false;
-    }
+    [SerializeField] private GameObject _leftBadge;
+    [SerializeField] private GameObject _rightBadge;
+    [SerializeField] private TMP_Text[] _streaks;
+    [SerializeField] private TMP_InputField[] _scores;
+    [SerializeField] private Image[] _voices;
+    
+    //MakeTeam - Prediction 관련 프로퍼티
+    [SerializeField] private bool _isRightClickEnabeled = false;
+    public Action<GameObject> OnRightClick;
+    public Role _role;
 
     private void OnEnable()
     {
-        StartAnimation();
+        foreach (var score in _scores)
+        {
+            score.onEndEdit.RemoveListener(OnInputFieldChanged);
+            score.onEndEdit.AddListener(OnInputFieldChanged);
+        }
     }
 
-    private void OnDisable()
+    private void OnInputFieldChanged(string value)
     {
-        StopAnimation();
-    }*/
+        foreach (var score in _scores)
+        {
+            score.text = value + "점";
+        }
+        if (value != "" && playerData.player != "-")
+        {
+            PlayerData data = new PlayerData();
+            data.scores = new PlayerData.Scores();
+            data.player = playerData.player;
+            float result;
+            switch (_role)
+            {
+                case Role.D:
+                    if (float.TryParse(value, out result))
+                    {
+                        data.scores.D = (int)(result * 100);
+                        data.scores.T = playerData.scores.T;
+                        data.scores.H = playerData.scores.H;
+                    }
+                    break;
+                case Role.T:
+                    if (float.TryParse(value, out result))
+                    {
+                        data.scores.D = playerData.scores.D;
+                        data.scores.T = (int)(result * 100);
+                        data.scores.H = playerData.scores.H;
+                    }
+                    break;
+                case Role.H:
+                    if (float.TryParse(value, out result))
+                    {
+                        data.scores.D = playerData.scores.D;
+                        data.scores.T = playerData.scores.T;
+                        data.scores.H = (int)(result * 100);
+                    }
+                    break;
+            }
+            Debug.Log($"{data.player} : {data.scores.D.ToString()}");
+            Debug.Log($"{data.player} : {data.scores.T.ToString()}");
+            Debug.Log($"{data.player} : {data.scores.H.ToString()}");
+            
+            StartCoroutine(DataController.instance.CreatePlayer(null, data));
+        }
+    }
+    public void SetBadgePosition(bool isLeft)
+    {
+        if (isLeft)
+        {
+            _leftBadge.SetActive(true);
+            _rightBadge.SetActive(false);
+        }
+        else
+        {
+            _leftBadge.SetActive(false);
+            _rightBadge.SetActive(true);
+        }
+
+        foreach (var streak in _streaks)
+        {
+            streak.text = "0";   
+        }
+        foreach (var score in _scores)
+        {
+            score.text = "0점";  
+        }
+    }
+    
+    public void SetBadge(int streak)
+    {
+        if (streak >= 0)
+        {
+            foreach (var _streak in _streaks)
+            {
+                _streak.text = streak.ToString();  
+            }
+        }
+        else
+        {
+            foreach (var _streak in _streaks)
+            {
+                _streak.text = streak.ToString();
+            }
+        }
+
+        float score = 0.0f;
+        float sValue = 0;
+        switch (_role)
+        {
+            case Role.D:
+                sValue = (float)playerData.scores.D / 100;
+                score = sValue;
+                break;
+            case Role.T:
+                sValue = (float)playerData.scores.T / 100;
+                score = sValue;
+                break;
+            case Role.H:
+                sValue = (float)playerData.scores.H / 100;
+                score = sValue;
+                break;
+        }
+        
+        foreach (var _score in _scores)
+        {
+            _score.text = (score % 1 == 0)
+                ? ((int)score) + "점"
+                : string.Format($"{score:0.##}") + "점";
+        }
+        
+        foreach (var _voice in _voices)
+        {
+            if (playerData.isVoiceAvailable.HasValue)
+            {
+                _voice.sprite = Resources.Load<Sprite>(
+                    playerData.isVoiceAvailable.Value 
+                        ? "Images/MicOn" 
+                        : "Images/MicOff"
+                );
+            }
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right && _isRightClickEnabeled)
+        {
+            OnRightClick?.Invoke(gameObject);
+        }
+    }
+
+    public PlayerData SetPlayerData(PlayerData newPlayerData)
+    {
+        playerData = newPlayerData;
+        _name.text = newPlayerData.player;
+        return playerData;
+    }
 }
